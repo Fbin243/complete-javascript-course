416785209ef8563e1148af16a584e9f2cee49095
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-05-28 22:45:04 +0700

    Finish section 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+'use strict';
 function calcSum(ope1, ope2) {
   return ope1 + ope2;
 }
@@ -12,3 +13,36 @@ const calcSum2 = function (ope1, ope2) {
 calcSum2(4, 5);
 
 console.log(typeof calcSum2);
+
+// Array
+const arr = [1, 2, 3, 'Hihi'];
+const arr2 = new Array(1, 2, 3, 'Hihi');
+
+
+// Object
+const jonas = {
+  firstName: 'Jonas',
+  lastname: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  hasDriverLicense: true,
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    this.calcAge();
+    console.log(`${this.firstName} is a ${this.age}-year old ${this.job}, and he has ${this.hasDriverLicense ? 'a' : 'no'} driver's license.`);
+  }
+}
+
+jonas.getSummary();
+
+
+const array = [];
+array[2] = 'Hihi';
+console.log(array);
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
