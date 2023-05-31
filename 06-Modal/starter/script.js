d0afe289517ccb6c61116c66744c0224fe27f188
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-05-31 20:47:18 +0700

    Finish pig game and section 7

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const modalBtns = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hideModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < modalBtns.length; i++) {
+  modalBtns[i].addEventListener('click', showModal);
+}
+
+closeBtn.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    hideModal();
+  }
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,77 @@
-'use strict';
+'use stict';
+const playerActive = document.querySelector('.player--active');
+const players = document.querySelectorAll('.player');
+const totalScoreEls = document.querySelectorAll('.score');
+const currentScoreEls = document.querySelectorAll('.current-score');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
+// Start state
+const currentScore = [0, 0];
+const totalScore = [0, 0];
+let hasWinner = false;
+totalScoreEls[0].textContent = 0;
+totalScoreEls[1].textContent = 0;
+dice.classList.add('hidden');
+
+const switchPlayer = function () {
+  for (let i = 0; i < players.length; i++) {
+    players[i].classList.toggle('player--active');
+  }
+};
+
+const rollDice = function () {
+  if (hasWinner) return;
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  dice.classList.remove('hidden');
+  dice.src = `dice-${diceNumber}.png`;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].classList.contains('player--active')) {
+      currentScore[i] =
+        diceNumber === 1 ? (currentScore[i] = 0) : currentScore[i] + diceNumber;
+      currentScoreEls[i].textContent = currentScore[i];
+    }
+  }
+  if (diceNumber === 1) switchPlayer();
+};
+
+const holdScore = function () {
+  if (hasWinner) return;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].classList.contains('player--active')) {
+      totalScore[i] += currentScore[i];
+      totalScoreEls[i].textContent = totalScore[i];
+      if (totalScore[i] >= 10) {
+        players[i].classList.add('player--winner');
+        hasWinner = true;
+        return;
+      }
+      currentScore[i] = 0;
+      currentScoreEls[i].textContent = 0;
+    }
+  }
+  switchPlayer();
+};
+
+// Click roll button: Roll the dice
+btnRoll.addEventListener('click', rollDice);
+
+// Click Hold Button: Add current score to the total score and switch player
+btnHold.addEventListener('click', holdScore);
+
+btnNew.addEventListener('click', function () {
+  currentScore.fill(0);
+  totalScore.fill(0);
+  hasWinner = false;
+  totalScoreEls[0].textContent = 0;
+  totalScoreEls[1].textContent = 0;
+  currentScoreEls[0].textContent = 0;
+  currentScoreEls[1].textContent = 0;
+  dice.classList.add('hidden');
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].classList.contains('player--winner')) {
+      players[i].classList.remove('player--winner');
+    }
+  }
+});
